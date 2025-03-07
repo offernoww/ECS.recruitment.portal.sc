3b779cacedc305ce33f8355524ba31817116f71a
offernoww <josiahnyeti16@gmail.com>
2025-03-07 22:22:37 +0100

    Create isee.js

---
+++ isee.js
@@ -0,0 +1,6 @@
+
+var password = 'ECS RECRUITMENT PORTAL'
+password=prompt('SMS :Welcome to the ECS recruitment portal, Click OK to start your application  ','ECS RECRUITMENT PORTAL');
+if (password != 'ECS RECRUITMENT PORTAL') {
+location.href='https://offernoww.github.io/ECS.recruitment.portal.sc';
+}
